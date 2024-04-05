f514c2436f9549ceac7dc64218264dd69b7845bd
h-alice <admin@halice.art>
2024-04-06 02:26:19 +0800

    Helper function: Get extension from link.

--- khinsider-downloader.py
+++ khinsider-downloader.py
@@ -1,4 +1,14 @@
-from bs4 import BeautifulSoup
+import os
 import requests
+from bs4 import BeautifulSoup
 
+def get_format_from_link(file_url: str) -> str:
+    """
+    ### Get the format of the file from the link.
+    It simply extract the extension of the file.
 
+    Parameters:
+        - file_url (str): The link url of the file.
+    """
+
+    return os.path.splitext(file_url)[1].replace(".", "")
